b7d72a937e0d09832653b3dd41176cec05292d23
albertopiffer <albertopiffer96@gmail.com>
2023-02-02 16:17:54 +0100

    FEAT esercizio completo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 for (let i = 1; i <= 100; i++) {
-    console.log(i)
+    
+    if (i % 15 == 0){
+        console.log('FizzBuzz')
+    }
+    else {
+        
+        if (i % 5 == 0 || i % 3 == 0) {
+            
+            if (i % 5 == 0){
+                console.log('Buzz')
+            }
+
+            if (i % 3 == 0){
+                console.log('Fizz')
+            }
+
+        }
+        else {
+            console.log(i)
+        }
+    }
 }
